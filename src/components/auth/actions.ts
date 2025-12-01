da21441b43e9b6ada6528bbc4e07c3dbde1542f7
곽태욱 <gwak2837@gmail.com>
2025-12-01 16:01:10 +0900

    chore: 이름 변경

--- src/components/auth/actions.ts
+++
@@ -1,7 +0,0 @@
-'use server'
-
-import { signIn } from '@/auth'
-
-export async function signInWithProvider(provider: 'google' | 'kakao' | 'naver') {
-  await signIn(provider, { redirectTo: '/dashboard' })
-}
