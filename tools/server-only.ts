e022fbe01519c2f304740f9aca791b3a6d2293b4
곽태욱 <gwak2837@gmail.com>
2025-12-10 23:03:32 +0900

    fix: 스크립트

--- tools/server-only.ts
+++ tools/server-only.ts
@@ -2,7 +2,13 @@ import { config } from 'dotenv'
 import Module from 'module'
 import { resolve } from 'path'
 
-const envFile = process.env.DB_ENV === 'test' ? '.env.test.local' : '.env.local'
+const envFile =
+  process.env.DB_ENV === 'test'
+    ? '.env.test.local'
+    : process.env.DB_ENV === 'production'
+      ? '.env.production.local'
+      : '.env.local'
+
 config({ path: resolve(process.cwd(), envFile), quiet: true })
 
 const originalRequire = Module.prototype.require
